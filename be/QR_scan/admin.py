83a8cbda7ac599bb182ec3cdbc13678b2d888cc6
Makememyday <congthinhluong.new@gmail.com>
2024-09-05 15:15:24 +0700

    add : admin user

--- be/QR_scan/admin.py
+++ be/QR_scan/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ProductData
 
 # Register your models here.
+@admin.register(ProductData)
+class ProductTrackingAdmin(admin.ModelAdmin):
+    list_display = ('product_id', 'destination', 'product', 'weight', 'weight_unit', 'value', 'value_unit', 'qr_data', 'recorded_at')
+    search_fields = ('product_id', 'destination', 'product', 'weight', 'weight_unit', 'value', 'value_unit', 'qr_data')
--- be/product_tracking/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProductTrackingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'product_tracking'
