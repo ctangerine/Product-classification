e8d3205fd04bcc9ce97cfc3a95aa25dad0eaefc9
Makememyday <congthinhluong.new@gmail.com>
2024-11-08 23:32:58 +0700

    add: product & receipt api

--- be/product_classification/settings.py
+++ be/product_classification/settings.py
@@ -49,7 +49,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # corsheaders
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'product_classification.urls'
 
--- be/ws_camera/models.py
+++ be/ws_camera/models.py
@@ -6,17 +6,17 @@ from django.db import models
 class Product(models.Model):
     product_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     product_name = models.CharField(max_length=100)
-    product_price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField()
+    product_price = models.FloatField()
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.product_name
 class Receipt(models.Model):
-    recept_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    product_name = models.CharField(max_length=100)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    receipt_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    product = models.ManyToManyField(Product)
+    total_price = models.FloatField(default=0)
     date = models.DateTimeField(auto_now_add=True)
+    data = models.JSONField(default=dict)
 
     def __str__(self):
         return self.recept_id
